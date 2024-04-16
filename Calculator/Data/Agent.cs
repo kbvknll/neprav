fabe07009467ef4b7379c97457da00e4ae4dfc69
Екатерина Гордеева <katiyhagordeeva@mail.ru>
2024-04-17 00:31:54 +0300

    kkkkkk

---
+++ Calculator/Data/Agent.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator.Data
+{
+    public class Agent
+    {
+        public Agent(string firstName, string lastName, int age, int group)
+        {
+            Id = Guid.NewGuid().ToString();
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Group = group;
+        }
+
+        public Agent()
+        {
+            Id = "";
+            FirstName = "";
+            LastName = "";
+            Age = 0;
+            Group = 0;
+        }
+
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Group { get; set; }
+    }
+}
---
+++ Calculator/Data/AgentService.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator.Data
+{
+    public class AgentService
+    {
+        public List<Agent> Agents = new List<Agent>()
+        {
+            new Agent("Ivan", "Ivanov", 18, 221),
+            new Agent("Ivan", "Petrov", 19, 321),
+            new Agent("Ivan", "Barinov", 18, 221),
+            new Agent("Ivan", "Shakirov", 18, 221),
+            new Agent("Ivan", "Elizarov", 19, 321),
+            new Agent("Ivan", "Skretov", 18, 221)
+        };
+    }
+}
