bdcee3b23cf0152b60df462f7883b73adbe07b7a
Екатерина Гордеева <katiyhagordeeva@mail.ru>
2024-04-17 04:05:46 +0300

    right

--- Calculator/Data/AgentService.cs
+++ Calculator/Data/AgentService.cs
@@ -1,12 +1,8 @@
-﻿using Calculator.Pages;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace Calculator.Data
+﻿namespace Calculator.Data
 {
     public class AgentService
     {
-        public List<Agent> Agents = new List<Agent>()
+        private List<Agent> Agents = new List<Agent>()
         {
             new Agent("Ivan", "Ivanov", 18, 221),
             new Agent("Ivan", "Petrov", 19, 321),
@@ -16,20 +12,50 @@ namespace Calculator.Data
             new Agent("Ivan", "Skretov", 18, 221)
         };
 
+        private List<Transaction> Transactions;
+
+        public AgentService()
+        {
+            Transactions = new List<Transaction>()
+            {
+                new Transaction(Agents.First(a => a.FirstName == "Ivan" && a.LastName == "Ivanov"), -1000),
+                new Transaction(Agents.First(a => a.FirstName == "Ivan" && a.LastName == "Petrov"), +1000),
+                new Transaction(Agents.First(a => a.FirstName == "Ivan" && a.LastName == "Barinov"), +2000),
+                new Transaction(Agents.First(a => a.FirstName == "Ivan" && a.LastName == "Shakirov"), -2000),
+                new Transaction(Agents.First(a => a.FirstName == "Ivan" && a.LastName == "Elizarov"), +2000),
+                new Transaction(Agents.First(a => a.FirstName == "Ivan" && a.LastName == "Skretov"), -3000),
+            };
+        }
+
+
         public Task<List<Agent>> GetAgentsAsync()
         {
-            // В реальном приложении вы бы запрашивали агентов из базы данных асинхронно.
-            // Для примера возвращаем асинхронно список агентов.
             return Task.FromResult(Agents);
         }
 
-        // Метод для добавления нового агента.
         public Task AddAgentAsync(Agent agent)
         {
-            // В реальном приложении вы бы добавляли агента в базу данных асинхронно.
-            // Для примера добавляем агента в список агентов.
             Agents.Add(agent);
             return Task.CompletedTask;
+        }
+
+        public Task<List<Transaction>> GetTransactionsAsync()
+        {
+            return Task.FromResult(Transactions);
+        }
+
+        public Task AddTransactionAsync(Agent agent, decimal amount)
+        {
+            Transactions.Add(new Transaction(agent, amount));
+            return Task.CompletedTask;
+        }
+
+        public async Task<decimal> GetAgentSumInDollarsAsync(string agentId)
+        {
+            decimal sum = Transactions
+                .Where(t => t.Agent.Id == agentId)
+                .Sum(t => t.Amount);
+            return sum;
         }
     }
 }
---
+++ Calculator/Data/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calculator.Data
+{
+    public class Transaction
+    {
+        public Agent Agent { get; set; }
+        public decimal Amount { get; set; }
+        public Transaction(Agent agent, decimal amount)
+        {
+            Agent = agent;
+            Amount = amount;
+        }
+
+        public Transaction()
+        {
+            Agent.Id = Guid.NewGuid().ToString();
+            Agent.FirstName = "";
+            Agent.LastName = "";
+            Amount = 0;
+        }
+    }
+}
